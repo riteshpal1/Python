5d7bb344f6ef282e8e9311a983c8809b7129b4d1
riteshpal1 <104260480+riteshpal1@users.noreply.github.com>
2022-09-06 18:26:38 +0530

    data science

--- Advanced/c6.py
+++ Advanced/c6.py
@@ -1,44 +1,47 @@
-''' wap to create a class Movie that have 
-properties like
--title
--rating
--year
-then create a constructor and a function to 
-display the info, Also create 5 movie object,
-store them in a list them in a list then sort 
-them by rating'''
+'''
+wap to create a class Movie
+that have properties like
+- title
+- rating
+- year
+then create a contructor and 
+a function to display the info.
+Also create 5 Movie object, store them in a list
+then sort them by rating.
+Easy peasy!
+'''
 
-class movie:
-  title = 'Jab tak hai jaan'
-  rating = '4.8'
-  year = '2016'
+class Movie:
+    def __init__(self, title, rating, year):
+        self.title = title
+        self.rating = rating
+        self.year = year
 
+    def info(self):
+        print(f'{self.title}({self.year}) -> {self.rating}')
 
-  def info(self):
-    return f'movie with {self.title}'
-    return f'movie with {self.rating}'
-    return f'movie with {self.year}'
+    # > overloaded
+    def __gt__(self, other):
+        if self.rating > other.rating:
+            return True
+        else:
+            return False
 
-class Rectangle(Shape):
+    def __lt__(self, other):
+        if self.rating < other.rating:
+            return True
+        else:
+            return False
 
-  def __init__(self, l, w):
-    super().__init__(l)
-    self.w = w 
+    def __repr__(self):
+        return self.title
 
+m = Movie('Jab Tak Hain Jaan', 4.8, 2013)
+m2 = Movie('Liger', 1, 2022)
 
-  def area(self):
-    return self.side * self.w   # side is taken as length
+print(m > m2)
+print(m2 > m)
 
-
-# overridden function
-  def info(self):
-    return f'rectangle with l = {self.side} & w = {self.w}'
-
-
-
-ob1 = shape(4)
-print(ob1.info())
-
-ob2 = Rectangle(Shape)
-print(ob2.area())
-print(ob2.info)
+Movie = [m, m2]
+Movie.sort()
+print(Movie)
